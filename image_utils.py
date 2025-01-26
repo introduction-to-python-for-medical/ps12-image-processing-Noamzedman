112e8bdd9f41f650c5c4e45e0c255e43444f4031
Noamzedman <Noamz440@gmail.com>
2025-01-26 16:54:25 +0200

    Update image_utils.py

--- image_utils.py
+++ image_utils.py
@@ -3,7 +3,11 @@ import numpy as np
 from scipy.signal import convolve2d
 
 def load_image(path):
-    pass # Replace the `pass` with your code
+    clean_image - suppress_noise(image_array)
+    edges = detect_edges(clean_image)
+    binary_edges = convert_to_binary(edges, threshold=50)
+    save_binary_image(binary_edges, 'my_edges.png')
 
 def edge_detection(image):
-    pass # Replace the `pass` with your code
+    edges = edge_detection(clean_image)
+    return edges
